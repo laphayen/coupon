4717ed13397977a5d1428312a33cbb4ba7b3003b
Baek Gi Chan <laphayen@gmail.com>
2025-04-02 23:47:30 +0900

    #1 - Chore: 다중 모듈 구성 및 공통 의존성 설정을 위한 Gradle 설정 리팩토링
    bootJar 비활성화 설정 (`bootJar.enabled = false`) 추가
    subprojects 블록 도입하여 공통 설정 분리 및 적용
    Lombok, JPA, Redis, H2, MySQL 관련 의존성 공통화
    Java 17 toolchain 명시적 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+val bootJar: org.springframework.boot.gradle.tasks.bundling.BootJar by tasks
+
+bootJar.enabled = false
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.3.10"
@@ -9,7 +13,13 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion.set(JavaLanguageVersion.of(17))
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
 	}
 }
 
@@ -17,12 +27,37 @@ repositories {
 	mavenCentral()
 }
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+subprojects {
+	apply(plugin = "java")
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+	group = rootProject.group
+	version = rootProject.version
+
+	java {
+		toolchain {
+			languageVersion.set(JavaLanguageVersion.of(17))
+		}
+	}
+
+	repositories {
+		mavenCentral()
+	}
+
+	dependencies {
+		implementation("org.springframework.boot:spring-boot-starter")
+		implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+		implementation("org.springframework.boot:spring-boot-starter-data-redis")
+		compileOnly("org.projectlombok:lombok")
+		annotationProcessor("org.projectlombok:lombok")
+		runtimeOnly("com.h2database:h2")
+		runtimeOnly("com.mysql:mysql-connector-j")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+		testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
 }
