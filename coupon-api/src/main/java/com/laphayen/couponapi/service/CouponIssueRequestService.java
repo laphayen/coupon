6053d88ab357dfb136dd54d7aa1071a3a886def6
Baek Gi Chan <laphayen@gmail.com>
2025-04-30 00:06:18 +0900

    #18 - Refactor: 쿠폰 발급 요청에 분산 락 적용하여 동시성 문제 해결
    기존 synchronized 블록을 제거하고 Redisson 기반 DistributeLockExecutor로 대체
    쿠폰별(lock+couponId)로 락을 획득하여 중복 발급 및 데이터 충돌 방지
    wait 시간과 lease 시간을 각각 10초로 설정하여 락 경합 상황에 대비
    고동시성 환경에서도 쿠폰 발급 로직의 정합성과 안정성 강화

--- coupon-api/src/main/java/com/laphayen/couponapi/service/CouponIssueRequestService.java
+++ coupon-api/src/main/java/com/laphayen/couponapi/service/CouponIssueRequestService.java
@@ -1,6 +1,7 @@
 package com.laphayen.couponapi.service;
 
 import com.laphayen.couponapi.controller.dto.CouponIssueRequestDto;
+import com.laphayen.couponcore.component.DistributeLockExecutor;
 import com.laphayen.couponcore.service.CouponIssueService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -13,12 +14,14 @@ public class CouponIssueRequestService {
 
     private final CouponIssueService couponIssueService;
 
+    private final DistributeLockExecutor distributeLockExecutor;
+
     private final Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
     public void issueRequestV1(CouponIssueRequestDto requestDto) {
-        synchronized (this) {
+        distributeLockExecutor.execute("lock" + requestDto.couponId(), 10000, 10000, ()  -> {
             couponIssueService.issue(requestDto.couponId(), requestDto.userId());
-        }
+        });
 
         log.info("쿠폰이 발급되었습니다. couponId: %s, userId: %s".formatted(requestDto.couponId(), requestDto.userId()));
     }
